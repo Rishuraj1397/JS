711b5091e3f19433317d4feb438352825b1761e8
Rishu Raj <143050346+Rishuraj1397@users.noreply.github.com>
2025-12-29 09:12:28 +0530

    update

---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 123456// this value can't be changed further in the code
+let accountEmail = "Rishu@hotmail"// this value can be changed further because we used let keyword which are used for declaring variable
+var accountPassword = "Rishu@123"// this value can be changed further because we used var keyword which are used for declaring variable 
+                                //but this method is outdated and not recommended to use in modern JS becasue of its function scope
+accountCity = "Bangalore"//this value is also a variable but we didn't use any keyword to declare it it is also acceptable in JS but not recommended
+let accountState;// in this we declared variable but didn't assigned any value so by default it will be undefined
+
+// accountId = 334455; // not allowed
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
