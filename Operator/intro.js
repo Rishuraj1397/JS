a59a402591cedd8201b50895bba27227f70d9ff0
Rishu Raj <rishuraj1397@gmail.com>
2026-06-03 14:40:16 +0530

    update

--- Operator/intro.js
+++ Operator/intro.js
@@ -110,11 +110,147 @@ let str  = String(n)
 console.log(str)
 console.log(typeof str)
 
-
+// classical error
 let one = 0.1
 let two = 0.2
+
 let three = Number((one + two).toFixed(1))
 
 console.log(three == 0.3)
+
+
+// 1: null is loosely equal to undefined only
+
+// console.log(null==undefined);
+// console.log(null===undefined);
+// console.log(null==0);
+// console.log(null=="");
+// console.log(null==false);
+// console.log(null==true);
+
+// >,<,>=,<= (null --> number, undefined --> NaN)
+
+// console.log(null>=0);
+// console.log(null<=0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null>=undefined);
+// console.log(undefined>=0);
+
+// console.log("Rohit">"Rahit");
+
+// console.log(10<true);
+
+// Ek koi bhi type hai, usko agar muje compare karna hai dusre kisi type se
+// Dono number mein convert honge
+
+// console.log(null>="");
+
+
+// console.log(NaN == NaN);
+
+// loop kaise kaam karte hai
+
+// for loop
+
+// post increment
+// post decrenment : i--
+
+// for(let i = 0; i<10;i++){
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 0;
+
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+
+// do while
+
+// let i= 0;
+
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+
+// if else condition
+
+// let age = 25;
+
+// if(age>=18){
+//     console.log("Eligible for vote");
+// }
+// else{
+//     console.log("Not eligible for vote");
+// }
+
+
+// kid young and old
+
+
+// let age = 78;
+
+// if(age<18){
+//     console.log("Kid");
+// }
+// else if(age>=60){
+//     console.log("Old");
+// }
+// else{
+//     console.log("young");
+// }
+
+
+
+// Logical Operator
+
+// logical And&& , || 
+
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&false);
+// console.log(false&&true);
+
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||false);
+// console.log(false||true);
+
+// let a = "Rohit";
+// let b = "";
+// let c = a&&b;
+// console.log(c);
+
+// let a = 0;
+// let b = 20;
+
+// console.log(a&&b);
+// &&: If first value is false, it will return the first value itself
+// If first value is true, it will return second
+
+
+// || logical or
+
+let a = 0;
+let b = 20;
+
+console.log(a||b);
+// &&: If first value is true, it will return the first value itself
+// If first value is false, it will return second
+
+// !=
+// console.log(5!=5);
+
+
+// AND: & , | or
+
+console.log(2&5);
 
 
